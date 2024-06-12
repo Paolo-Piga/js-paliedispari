667eb216f0ed19611b831b68339ea02c718f5362
DecimTheJudge <paolo1piga@gmail.com>
2024-06-12 21:19:49 +0200

    finito

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// propmt per fare inserire all'utente pari o dispari
+let UserChoice = prompt("Scrivi `pari` o `dispari`")
+console.log("Hai fatto la tua scelta");
+
+// propmt per fare inserire un numero da 1 a 5 all'utente
+let Usernumero = parseInt(prompt("Inserisci un numero da 1 a 5"));
+console.log("Hai scelto il numero:", Usernumero );
+
+// funzione per generare un numero random per il pc tra 1 e 5 
+function PcChoice() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+let PcChoiceNumber = PcChoice() ;
+console.log("Il Pc ha scelto:", PcChoiceNumber);
+
+// metto una condizione che dichiara che se il pc ha messo dispari o pari vince e stessa cosa per l'utente
+
+if{
+
+}if else{
+    
+}
